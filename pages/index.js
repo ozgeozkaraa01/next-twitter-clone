f7ee308026962bf46ff8d32dbed0fbd7e5d0c43e
Özgenur Özkara <ozgenurozkara@gmail.com>
2023-07-12 14:02:10 +0300

    create feed component and its header

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Sidebar from "../components/Sidebar";
+import Feed from "../components/Feed";
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
         <Sidebar />
 
         {/* Feed */}
+        <Feed />
 
         {/* Widgets */}
 
